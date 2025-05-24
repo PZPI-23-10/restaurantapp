7225e703ba241f6e1dba2083f26b1780b67df551
KovalenkoM06 <makskovalenko2099@gmail.com>
2025-05-24 13:26:27 +0300

    MaksimStart

---
+++ Platforms/Android/WebAuthenticationCallbackActivity.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Microsoft.Maui.Authentication;
+
+namespace RestoranApp
+{
+    [Activity(NoHistory = true, LaunchMode = LaunchMode.SingleTop, Exported = true)]
+    [IntentFilter(
+         new[] { Intent.ActionView },
+         Categories = new[] {
+            Intent.CategoryDefault,
+            Intent.CategoryBrowsable
+         },
+         DataScheme = "com.companyname.restoranapp",
+         DataPath = "/auth")]
+    public class WebAuthenticationCallbackActivity : WebAuthenticatorCallbackActivity
+    {
+    }
+}
---
+++ obj/Debug/net8.0-android/android/src/crc640d93a7dcf3447389/WebAuthenticationCallbackActivity.java
@@ -0,0 +1,39 @@
+package crc640d93a7dcf3447389;
+
+
+public class WebAuthenticationCallbackActivity
+	extends crc6468b6408a11370c2f.WebAuthenticatorCallbackActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("RestoranApp.WebAuthenticationCallbackActivity, RestoranApp", WebAuthenticationCallbackActivity.class, __md_methods);
+	}
+
+
+	public WebAuthenticationCallbackActivity ()
+	{
+		super ();
+		if (getClass () == WebAuthenticationCallbackActivity.class) {
+			mono.android.TypeManager.Activate ("RestoranApp.WebAuthenticationCallbackActivity, RestoranApp", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
